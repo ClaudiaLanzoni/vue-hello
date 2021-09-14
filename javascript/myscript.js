9bf215c1825d5575dcbe791858080e19e780a7ae
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-09-14 17:38:24 +0200

    exercise structure

---
+++ javascript/myscript.js
@@ -0,0 +1,8 @@
+let app = new Vue ({
+    el : '#root',
+    data : {
+        message: 'Hello!!!',
+        imgURL : 'https://it.wikipedia.org/wiki/Premio_Tenco#/media/File:Luigi_tenco.jpg',
+        image: 'https://www.gossipetv.com/wp-content/uploads/2017/04/beautiful-ronn-moss-oggi.jpg'
+    }
+})
